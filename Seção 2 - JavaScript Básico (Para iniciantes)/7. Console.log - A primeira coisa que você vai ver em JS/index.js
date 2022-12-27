cb2ad4eb3efd44a68111da1e1391c13f7383eefa
Matheus Carvalho <matheusluancarvalho@gmail.com>
2022-12-27 16:30:22 -0300

    Aula 01

---
+++ Seção 2 - JavaScript Básico (Para iniciantes)/7. Console.log - A primeira coisa que você vai ver em JS/index.js
@@ -0,0 +1,11 @@
+console.log('Hello World');
+console.log("Matheus Carvalho");
+console.log(`Matheus Carvalho`);
+
+console.log("Matheus 'Carvalho'");
+console.log('Matheus "Carvalho"');
+console.log(`'Matheus' "Carvalho"`);
+
+console.log(123456789);
+console.log(1 + 1);
+console.log(123, 15.6, 'Matheus Carvalho');
