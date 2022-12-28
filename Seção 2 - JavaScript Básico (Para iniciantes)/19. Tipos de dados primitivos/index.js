a103457fa92dc5b0c468952eda3aae4da0e25f62
Matheus Carvalho <matheusluancarvalho@gmail.com>
2022-12-28 11:05:42 -0300

    Aula 09

---
+++ Seção 2 - JavaScript Básico (Para iniciantes)/19. Tipos de dados primitivos/index.js
@@ -0,0 +1,34 @@
+// String
+
+const nome = 'Matheus';
+const nome1 = "Matheus";
+const nome2 = `Matheus`;
+
+// Number
+
+const numero = 10;
+const numero1 = 10.8;
+
+// Boolean
+
+const boolean1 = true;
+const boolean2 = false;
+
+// Undefined
+
+let nomeCliente; // Não aponsta em nenhum local na memória
+
+// Null
+
+let sobrenomeCliente = null; // Não aponsta em nenhum local na memória
+
+// Exemplo
+
+let a = 2;
+let b  = a;
+
+console.log(a, b);
+
+a = 3;
+
+console.log(a, b);
