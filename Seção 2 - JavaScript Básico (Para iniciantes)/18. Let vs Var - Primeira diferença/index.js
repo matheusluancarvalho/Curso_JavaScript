ed3df7704773f820718e575d384bc1393852bd27
Matheus Carvalho <matheusluancarvalho@gmail.com>
2022-12-28 10:48:19 -0300

    Aula 08

---
+++ Seção 2 - JavaScript Básico (Para iniciantes)/18. Let vs Var - Primeira diferença/index.js
@@ -0,0 +1,14 @@
+// Var aceita fazer redeclaração
+
+var nome = 'Matheus';
+var nome = 'Carvalho';
+
+console.log(nome);
+
+// Em let não se pode fazer redaclaração de variáveis
+
+let nomean = 'Matheus';
+//let nome = 'Carvalho'; -> Forma errada
+nomean = 'Carvalho';
+
+console.log(nomean);
