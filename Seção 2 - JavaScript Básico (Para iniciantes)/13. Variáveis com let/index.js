28f2763464fb348743904ec00a1c5828a939cf72
Matheus Carvalho <matheusluancarvalho@gmail.com>
2022-12-28 09:38:07 -0300

    Aula 05

---
+++ Seção 2 - JavaScript Básico (Para iniciantes)/13. Variáveis com let/index.js
@@ -0,0 +1,23 @@
+let nome = 'João';
+
+console.log(nome,'nasceu em 1984.');
+console.log('Em 2000,',nome,'conheceu Maria.');
+console.log(nome,'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com',nome,'em 2015.');
+console.log('O filho de',nome,'se chama Eduardo.');
+console.log('--------------------------------------------------------------');
+
+let outro_nome;
+outro_nome = 'Matheus';
+console.log(outro_nome);
+
+//Não pode redeclarar variáveis. Exemplo: let nome = 'Matheus' & let nome = 'João'
+
+// Regras
+/*
+1 - Não podemos criar variáveis com palavras reservadas
+2 - Variáveis precisam fazer sentido e ter significados
+3 - Não pode começar uma variável com um número
+4 - Não podem conter espaços e traços
+5 - Utilizamos camelCase
+6 - Case-sensitive */
