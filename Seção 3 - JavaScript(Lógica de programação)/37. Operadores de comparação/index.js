4a89f0664b7d0a5a672dafafd43beeac7a548fac
Matheus Carvalho <matheusluancarvalho@gmail.com>
2023-01-02 16:10:55 -0300

    Aula 01

---
+++ Seção 3 - JavaScript(Lógica de programação)/37. Operadores de comparação/index.js
@@ -0,0 +1,22 @@
+/*
+
+                            Comparadores de Comparação
+
+Maior que ... >
+Maior que ou igual a ... >=
+Menor que ... <
+Menor que ou igual a ... <=
+Igualdade ... == (valor)
+Igualdade estrita ... === (valor e tipo)
+Diferente ... != (valor)
+Diferente estrito ... !== (valor e tipo)
+
+*/
+
+console.log(10 > 5);   // Dez é maior que cinco
+console.log(10 >= 5); // Dez é maior ou igual a cinco
+
+console.log(10 == 10);
+console.log(10 === 10);  // Checa além do valor, checa o tipo
+
+console.log(10 !== 10); // false
